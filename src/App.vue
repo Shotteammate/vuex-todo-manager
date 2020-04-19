2dd6daff463480f37637655534789311b4c132ce
shotteammate <shotteammate@gmail.com>
2020-04-19 12:33:37 +0800

    chore: setup vuex environment

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
-    todo
+    <div class="container">
+      <TodoList />
+    </div>
   </div>
 </template>
 
 <script>
-
+import TodoList from "./components/TodoList";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  
+    TodoList
   }
-}
+};
 </script>
 
 <style>
 body {
   font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
-  margin: 0;
-  padding: 0;
+  line-height: 1.6;
+  background: #e8f7f0;
 }
-
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>todo list</h3>
+    <div class='todolist'>
+      <div v-for="todo in allTodos" :key='todo.id' class='todo'>
+        {{todo.title}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "todolist",
+  computed: mapGetters(['allTodos'])
+};
+</script>
+
+<style>
+</style>
